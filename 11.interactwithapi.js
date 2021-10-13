e3efa5706b3820cc6ec0c126aa09e3e4fc644f7a
nagadeepak511 <nagadeepak511@gmail.com>
2021-10-13 14:00:01 +0530

    fetch using async/await

--- 11.interactwithapi.js
+++ 11.interactwithapi.js
@@ -1,3 +1,5 @@
+// fetch then
+/*
 function getAllPokemons(){
     fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100')
     .then((res)=>{return res.json()})
@@ -17,3 +19,25 @@ function getAllPokemons(){
         })
     })
 }
+
+*/
+
+async function getAllPokemons(){
+    var res = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100')
+    var data = await res.json()
+    var i=1;
+        document.getElementsByClassName('row')[3].innerHTML = ''
+        data.results.map((item)=>{
+            document.getElementsByClassName('row')[3].innerHTML += 
+            `
+            <div class="pokemon-container col-md-2 col-sm-3 col-xs-4">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" alt="" class="pokemon-image">
+                    <div class="pokemon-number">#${i}</div>
+                    <div class="pokemon-name">${item.name}</div>
+            </div>
+            `
+            i++
+        })
+}
+
+getAllPokemons()
